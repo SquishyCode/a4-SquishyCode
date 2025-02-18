8d3286e5d61a1c94b80264b4208ac97d2aa35350
SquishyCode <mjmergal13@gmail.com>
2025-02-18 19:17:38 -0400

    pleas peasleae please plaese

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "https://a4-squishycode.onrender.com",
+    port: 5173,
     cors: true,
     historyApiFallback: true,
     allowedHosts: true
