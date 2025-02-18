c67e95d3aa0a4220813c53d5d4cb197b105e75ba
SquishyCode <mjmergal13@gmail.com>
2025-02-18 18:34:53 -0400

    cyber security is cool

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: "https://a4-squishycode.onrender.com",
     cors: true,
     historyApiFallback: true,
     allowedHosts: true
