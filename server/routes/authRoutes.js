0f77b2e4339aa318d56f575648d80771e49f599d
SquishyCode <mjmergal13@gmail.com>
2025-02-18 23:28:09 -0400

    please^20 please (cred true) please pppppppppp

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -72,7 +72,7 @@ module.exports = (usersCollection, dataCollection) => {
             //     return res.status(404).json({ message: "User not found" });
             // }
 
-            res.status(200).json({ userData, user: { _id: user._id, username: user.username } });
+            res.json({ userData, user: { _id: user._id, username: user.username } });
         } catch (error) {
             console.error("Error fetching results:", error);
             res.status(500).json({ message: "Internal Server Error" });
